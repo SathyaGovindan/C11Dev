a0e9b99d8f89792d464699db32fccd691eb29d04
Pirya <priyagovindhan92@gmail.com>
2021-09-12 14:58:59 +0530

    added route to list page comopnent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListUserComponent } from './list-user/list-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/users', pathMatch: 'full'},
+  { path: 'users', component: ListUserComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
